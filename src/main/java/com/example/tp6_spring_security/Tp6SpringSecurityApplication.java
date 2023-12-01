fc6488256b96d7b6a1affd733d61843d6882fd4e
khalilMaat <maatoug620@outlook.fr>
2023-12-01 12:01:42 +0100

    Spring Security with 2 method

---
+++ src/main/java/com/example/tp6_spring_security/Tp6SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp6_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp6SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp6SpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tp6_spring_security/Tp6SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tp6_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp6SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
